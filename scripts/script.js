fb7cc1c48030b6f9f489225513a0ed43d218f80b
moheywael <mohey.wael2020@gmail.com>
2023-03-13 21:23:46 +0200

    sidebar fully funcational

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 // variables Sidebar
 let sidebar = document.querySelector(".sidebar")
-let sidebar_subMenu = document.querySelector(".sidebar__body__sub-menu")
+
+let sidebar_subMenu = document.querySelectorAll(".sidebar__body__sub-menu")
+let sidebar_subMenu_btn = document.querySelectorAll(".sidebar__body__menu__btn")
+let sidebar_subMenu_elearning = document.querySelector(".elearning-sub-menu")
+let sidebar_subMenu_semesterwork = document.querySelector(".semesterwork-sub-menu")
+
+
 let sidebar_toggle = document.querySelector(".sidebar__header__toggle")
+let sidebar_activityCenter = document.querySelector(".activity-center")
 
 // variables - xxx
 
@@ -9,21 +16,46 @@ let sidebar_toggle = document.querySelector(".sidebar__header__toggle")
 
 // Event Listeners - Sidebar
 sidebar_toggle.addEventListener("click", collapse)
-sidebar_subMenu.addEventListener("click", expand)
+sidebar_activityCenter.addEventListener("click", activiryExpand)
+
 
 // Event Listeners - Sidebar
+sidebar_subMenu_btn.forEach(subMenu => {
+    subMenu.addEventListener("click", () => {
+        // E-Learning Sub Menu
+        if (subMenu.classList.contains("sidebar__body__menu__elearning-btn")) {
+            sidebar_subMenu_elearning.classList.toggle("sidebar__body__sub-menu--opened")
+        }
+
+        // Semester Work Sub Menu
+        if (subMenu.classList.contains("sidebar__body__menu__semesterwork-btn")) {
+            sidebar_subMenu_semesterwork.classList.toggle("sidebar__body__sub-menu--opened")
+        }
+
+        subMenu.querySelectorAll("i.fa-solid")[1].classList.toggle("rotate")
+
+    })
+})
+
 
 
 
 // Functions
 function collapse() {
     sidebar.classList.toggle("collapsed")
-    sidebar_toggle.classList.toggle("sidebar__header__toggle--active")
-    // activityCenter.classList.toggle("")
-    sidebar_subMenu.classList.remove("active")
+    sidebar_toggle.classList.toggle("rotate")
+
+    // sidebar_activityCenter.classList.remove("activity-center--active")
+
+    sidebar_subMenu.forEach(subMenu => {
+        subMenu.classList.remove("sidebar__body__sub-menu--opened")
+        // console.log(document.querySelectorAll(".sidebar__body__sub-menu--opened"))
+    })
 }
 
-function expand() {
-    sidebar_subMenu.classList.toggle("active")
-
+function activiryExpand() {
+    sidebar_activityCenter.classList.toggle("activity-center--active")
 }
+
+// function expand() {}
+
